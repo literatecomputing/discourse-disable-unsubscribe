4fc6d4217db6c001e38cfd3c860082a26b626b23
Jay Pfaffman <pfaffman@gmail.com>
2021-08-09 08:22:36 -0700

    override builder helper

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 # name: discourse-plugin-name
-# about: TODO
+# about: Remove unsubscribe links for members of a particular group
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Jay Pfaffman
+# url: https://github.com/literatecomputing/discourse-disable-unsubscribe
 # required_version: 2.7.0
-# transpile_js: true
+# transpile_js: false
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :no_unsubscribe_enabled
 
 after_initialize do
+  load File.expand_path('extensions/build_email_helper.rb', __dir__)
+  ::Email::BuildEmailHelper.prepend BuildEmailHelperExtension
 end
