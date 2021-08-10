73be4bb2f62ac73f6c3d592ba66ea4c9aba542b8
Jay Pfaffman <pfaffman@gmail.com>
2021-08-10 09:05:50 -0700

    add debug log

--- extensions/build_email_helper.rb
+++ extensions/build_email_helper.rb
@@ -4,6 +4,7 @@ module BuildEmailHelperExtension
     # if email-user is in the group, set add_unsubscribe_link false
     user = User.find_by_email(email)
     remove = user.groups.pluck(:name).include? SiteSetting.no_unsubscribe_group
+    Rails.logger.warn("BuildEmailHelperExtension. User: #{user.username}. Remove: #{remove}")
     opts[:add_unsubscribe_link] = false if remove
     super(email, opts)
   end
